bc0df144994056dda692a818b101b9d9f3823ece
Tilo <166017922+Azurtilo@users.noreply.github.com>
2024-08-30 10:24:43 -0400

    Revert "app3 terminadav2"

--- app3/views.py
+++ app3/views.py
@@ -10,7 +10,6 @@ def presentacion3(request):
         <h1 style="color:red">Soy un titulo de la app3.</h1>
         <h2 style="color:green">Soy un subtitulo de la app3</h2>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/iQN0z6MDrEY?si=nuzoinbB382SQsbx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <p>hola</p>
     """
     return HttpResponse(html)
 
